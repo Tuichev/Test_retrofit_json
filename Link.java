4f5fb29b68d07433c89b81b2c1521bf85ffca2cf
Tuichev <vtuichev@gmail.com>
2017-05-28 22:46:55 +0300

    Update Link.java

--- Link.java
+++ Link.java
@@ -1,14 +1,10 @@
 package com.xipypr.translategoogle;
 
 import java.util.Map;
-
 import retrofit2.Call;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-
-
-
 
 public interface Link {
     @FormUrlEncoded
